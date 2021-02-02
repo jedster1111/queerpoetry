c0f7f977ba20e80d888c3120eab71bc4eee5ded9
Jed Thompson <jedster1111@hotmail.co.uk>
2021-02-02 01:58:10 +0000

    Basic layout of poet list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { v4 as uuid } from "uuid";
+import "./App.css";
+import { names } from "./names";
+
+type Poet = { id: string; name: string; portraitUrl: string };
+
+type PoetsListProps = {
+  poets: Poet[];
+};
+
+const PoetItem = ({ poet }: { poet: Poet }) => (
+  <div className="PoetItem">
+    <div className="PoetItem-content">
+      <h2 className="PoetItem-name">{poet.name}</h2>
+    </div>
+  </div>
+);
+
+const PoetsList = ({ poets }: PoetsListProps) => (
+  <div className="PoetsList">
+    {poets.map((poet) => (
+      <PoetItem key={poet.id} poet={poet} />
+    ))}
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Queer Poetry</header>
+      <main className="App-body">
+        <PoetsList poets={names.map(createPoet)} />
+      </main>
     </div>
   );
 }
 
 export default App;
+
+function createPoet(name: string): Poet {
+  return {
+    id: uuid(),
+    name,
+    portraitUrl:
+      "https://nypost.com/wp-content/uploads/sites/2/2020/01/peter-chinman-by-michael-j.-fiedler.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+  };
+}
