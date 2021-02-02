13bc4c431baf976b4d01ee906d032555b69de778
Jed Thompson <jedster1111@hotmail.co.uk>
2021-02-02 03:13:32 +0000

    Fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders Queer Poetry header", () => {
   render(<App />);
-  const Header = screen.getByText(/Queer Poetry/i);
+  const Header = screen.getByText(/Queer Poets/i);
   expect(Header).toBeInTheDocument();
 });
